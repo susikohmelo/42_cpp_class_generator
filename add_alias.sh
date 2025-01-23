e56ac861ad932746151fa344e92f1141ae2b8dfd
susikohmelo <lassi.jylhankangas@gmail.com>
2025-01-23 05:01:58 +0200

    very silly misplacement of the newline fixed in add_alias

--- add_alias.sh
+++ add_alias.sh
@@ -3,8 +3,8 @@
 # All this does is append the alias to the .zshrc and .bashrc files
 
 SCRIPT_DIR=$(pwd)
-printf "\nalias cgen=\"%s\"" "${SCRIPT_DIR}/cgen.sh\n" >> "${HOME}/./.bashrc"
-printf "\nalias cgen=\"%s\"" "${SCRIPT_DIR}/cgen.sh\n" >> "${HOME}/./.zshrc"
+printf "\nalias cgen=\"%s\"\n" "${SCRIPT_DIR}/cgen.sh" >> "${HOME}/./.bashrc"
+printf "\nalias cgen=\"%s\"\n" "${SCRIPT_DIR}/cgen.sh" >> "${HOME}/./.zshrc"
 
 echo -e "\e[32mExit & reopen the terminal for alias to take effect\e[0m"
 echo -e "\e[33mThe alias name is cgen\e[0m"
