3f1bce207d072e91b178cb37dfa89586c95e5d29
susikohmelo <lassi.jylhankangas@gmail.com>
2025-01-23 04:52:49 +0200

    added newline to add_alias.sh

--- add_alias.sh
+++ add_alias.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# All this does is append the alias to the .zshrc and .bashrc files
+
 SCRIPT_DIR=$(pwd)
-printf "alias cgen=\"%s\"" "${SCRIPT_DIR}/cgen.sh" >> "${HOME}/./.bashrc"
-printf "alias cgen=\"%s\"" "${SCRIPT_DIR}/cgen.sh" >> "${HOME}/./.zshrc"
+printf "\nalias cgen=\"%s\"" "${SCRIPT_DIR}/cgen.sh\n" >> "${HOME}/./.bashrc"
+printf "\nalias cgen=\"%s\"" "${SCRIPT_DIR}/cgen.sh\n" >> "${HOME}/./.zshrc"
 
 echo -e "\e[32mExit & reopen the terminal for alias to take effect\e[0m"
+echo -e "\e[33mThe alias name is cgen\e[0m"
